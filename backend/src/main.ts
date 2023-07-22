71bc89d898320979a8c14652c1da0ee6ccb2755d
Bashakin <bashakin@greensight.ru>
2023-07-22 17:38:49 +0300

    fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import {ConfigService} from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const corsOptions = {
+    origin: configService.get<string | string[]>('allowedOrigins'),
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    allowedHeaders: ['authorization', 'content-type'],
+  };
+  app.enableCors(corsOptions);
+  app.useGlobalPipes(new ValidationPipe());
+  const PORT = 3000
+  await app.listen(PORT);
 }
-bootstrap();
+
+bootstrap().then(() => console.log('Started'));
